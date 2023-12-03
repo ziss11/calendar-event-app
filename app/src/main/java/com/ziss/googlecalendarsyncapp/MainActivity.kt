837d3fdad7e7491a333ab2c533260695c3f61fcd
Abdul Azis <abdul.azizz1108@gmail.com>
2023-12-03 20:36:37 +0700

    change app bar title

--- app/src/main/java/com/ziss/googlecalendarsyncapp/MainActivity.kt
+++ app/src/main/java/com/ziss/googlecalendarsyncapp/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.title = getString(R.string.event)
+        supportActionBar?.title = getString(R.string.calendar_event)
 
         loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
